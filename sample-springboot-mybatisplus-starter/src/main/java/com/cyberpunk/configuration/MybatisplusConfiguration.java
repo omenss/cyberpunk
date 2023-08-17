4054feb4f980c7f98de80534afd7a5458216ff63
lujun <jun.lu@ncsi.com.cn>
2023-08-17 18:15:29 +0800

    安全依赖问题解决CVE

---
+++ sample-springboot-mybatisplus-starter/src/main/java/com/cyberpunk/configuration/MybatisplusConfiguration.java
@@ -0,0 +1,11 @@
+package com.cyberpunk.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lujun
+ * @date 2023/8/17 18:08
+ */
+@Configuration
+public class MybatisplusConfiguration {
+}
