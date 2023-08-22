c56b3df846a486d08e67033652b439cf57d9da78
lujun <jun.lu@ncsi.com.cn>
2023-08-22 15:08:59 +0800

    canal

---
+++ demo-elk-log/src/main/java/com/cyberpunk/elk/log/ElkApplication.java
@@ -0,0 +1,15 @@
+package com.cyberpunk.elk.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lujun
+ * @date 2023/8/22 14:53
+ */
+@SpringBootApplication
+public class ElkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElkApplication.class, args);
+    }
+}
